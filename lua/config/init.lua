fce4a8f123bbef5819db7efefe6d452361da8fec
codehia <dev@sacharya.dev>
2024-06-05 15:56:47 +0530

    LSP

---
+++ lua/config/init.lua
@@ -0,0 +1,4 @@
+require 'config.global_options'
+require 'config.options'
+require 'config.keymaps'
+require 'config.autocommand'
---
+++ lua/plugins/conform.lua
@@ -0,0 +1,59 @@
+local slow_format_filetypes = {}
+return { -- Autoformat
+
+  'stevearc/conform.nvim',
+  lazy = false,
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true, lsp_fallback = true }
+      end,
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+  },
+  opts = {
+    notify_on_error = true,
+    format_after_save = function(bufnr)
+      if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+        require('conform').format { bufnr = bufnr, async = true }
+      end
+      return { lsp_fallback = true }
+    end,
+    format_on_save = function(bufnr)
+      -- Disable "format_on_save lsp_fallback" for languages that don't
+      -- have a well standardized coding style. You can add additional
+      -- languages here or re-enable it for the disabled ones.
+      local disable_filetypes = { c = true, cpp = true }
+      if slow_format_filetypes[vim.bo[bufnr].filetype] then
+        return
+      end
+
+      local function on_format(err)
+        if err and err:match 'timeout$' then
+          slow_format_filetypes[vim.bo[bufnr].filetype] = true
+        end
+      end
+
+      return {
+        timeout_ms = 500,
+        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        on_format,
+      }
+    end,
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      -- Conform can also run multiple formatters sequentially
+      -- python = { 'isort', 'autopep8' },
+      -- python = { "isort", "black" },
+      --
+      -- You can use a sub-list to tell conform to run *until* a formatter
+      -- is found.
+      -- javascript = { { "prettierd", "prettier" } },
+    },
+    formatters = {
+      autopep8 = { args = { '--max-line-length=120' } },
+    },
+  },
+}
