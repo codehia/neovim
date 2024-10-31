8b35080ffcbff0649884dea83a1324ef8944faa7
codehia <dev@sacharya.dev>
2024-10-31 18:45:30 +0530

    Update which-key config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -202,9 +202,6 @@ return { -- LSP Configuration & Plugins
       --    https://github.com/pmizio/typescript-tools.nvim
       --
       -- But for many setups, the LSP (`tsserver`) will work just fine
-      tsserver = {},
-      --
-
       lua_ls = {
         -- cmd = {...},
         -- filetypes = { ...},
