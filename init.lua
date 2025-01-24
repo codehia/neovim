8ff020fd1859811505dee3e38dfdb1bd59e327d3
codehia <dev@sacharya.dev>
2025-01-24 18:40:28 +0530

    Clean up

--- init.lua
+++ init.lua
@@ -1,4 +1,1 @@
-require 'config'
-require('lazy').setup('plugins', require 'lazy_config')
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+require 'config.lazy'
--- lua/plugins/copilot_chat.lua
+++ lua/plugins/copilot_chat.lua
@@ -3,13 +3,8 @@ return {
     'CopilotC-Nvim/CopilotChat.nvim',
     branch = 'main',
     dependencies = {
-      { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
-      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+      { 'zbirenbaum/copilot.lua' },
+      { 'nvim-lua/plenary.nvim' },
     },
-    opts = {
-      -- debug = true, -- Enable debugging
-      -- See Configuration section for rest
-    },
-    -- See Commands section for default commands if you want to lazy load on them
   },
 }
